d6b3f3fa6be6970b717f3a6f8086c5c9cf3a7adf
hung1702 <phantrunghung1702@gmail.com>
2023-05-05 17:04:53 +0700

    initial

---
+++ QLDTMobile/QLDTMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using QLDTMobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace QLDTMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ QLDTMobile/QLDTMobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using QLDTMobile.Models;
+using QLDTMobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QLDTMobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
