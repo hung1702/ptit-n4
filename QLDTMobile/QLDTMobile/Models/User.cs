4ec27b4e006c041233dd534d2c2dd3ef76ea9dc9
hung1702 <phantrunghung1702@gmail.com>
2023-05-11 08:30:03 +0700

    second commit

---
+++ QLDTMobile/QLDTMobile/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLDTMobile.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Ten { get; set; }
+        public int Tuoi { get; set; }
+        public string DiaChi { get; set; }
+        public bool GioiTinh { get; set; }
+        public string SDT { get; set; }
+        public string SCMT { get; set; }
+        public string Email { get; set; }
+    }
+}
--- QLDTMobile/QLDTMobile/ViewModels/LoginViewModel.cs
+++ QLDTMobile/QLDTMobile/ViewModels/LoginViewModel.cs
@@ -1,6 +1,8 @@
-﻿using QLDTMobile.Views;
+﻿using QLDTMobile.Models;
+using QLDTMobile.Views;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xamarin.Forms;
 
@@ -10,15 +12,65 @@ namespace QLDTMobile.ViewModels
     {
         public Command LoginCommand { get; }
 
-        public LoginViewModel()
+        public Command ForgotPasswordCommand { get; }
+
+        private string _username = "admmin";
+        public string Username
         {
-            LoginCommand = new Command(OnLoginClicked);
+            get { return _username; }
+            set { SetProperty(ref _username, value); }
         }
 
-        private async void OnLoginClicked(object obj)
+        private string _password = "abc";
+        public string Password
         {
-            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        private bool _forgotPassword = false;
+        public bool ForgotPassword
+        {
+            get { return _forgotPassword; }
+            set { SetProperty(ref _forgotPassword, value); }
+        }
+
+        public LoginViewModel()
+        {
+            Username = "admin";
+            Password = "123";
+            ForgotPassword = false;
+            LoginCommand = new Command(OnLoginClicked);
+
+            if (ForgotPassword)
+            {
+                ForgotPasswordCommand = new Command(OnForgotPasswordClicked);
+            }
+        }
+
+        public async void OnLoginClicked(object obj)
+        {
+            var accountList = new List<Account>()
+            {
+                new Account() {Id = Guid.Empty, UserName = "admin", Password = "admin", Email = "admin@gmail.com"},
+                new Account() {Id = Guid.Empty, UserName = "admin1", Password = "admin1", Email = "admin1@gmail.com"},
+                new Account() {Id = Guid.Empty, UserName = "admin2", Password = "admin2", Email = "admin2@gmail.com"},
+            };
+
+            if (!string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password))
+            {
+                var user = accountList.FirstOrDefault(a => a.UserName == Username);
+                if(user != null && user.Password.Equals(Password))
+                {
+                    // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+                    await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+                }
+            }
+        }
+
+        public async void OnForgotPasswordClicked()
+        {
+
         }
     }
 }
